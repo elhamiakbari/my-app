7c3b46d9cd7c8f7e6407d691a5f0d2eb14f08d93
elhamiakbari <e.akbari1992@gmail.com>
2022-01-30 12:47:58 +0330

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Search from "./Search";
+import Weather from "./Weather";
 
-function App() {
+import "./App.css";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="App">
+        <Search />
+        <ul className="float-left">
+          <li className="city">New York </li>
+          <li className="info">Last updated:Tuesday 10:00 </li>
+          <li className="info">Cloudy</li>
+        </ul>
+        <Weather />
+      </div>
     </div>
   );
 }
-
-export default App;
